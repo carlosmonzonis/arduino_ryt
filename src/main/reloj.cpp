f431778ba4f3884969f402eb7cd94bd4de41e2d0
Carlos <carlosmonzonis@outlook.es>
2021-12-16 21:44:32 +0100

    cambios en la escritura de la fecha

---
+++ src/main/reloj.cpp
@@ -0,0 +1,59 @@
+#include "reloj.h"
+
+RTC_DS1307 rtc;
+
+void reloj::check_status(){
+  if (!rtc.begin()) {
+    Serial.println("****** Couldn't find RTC ******");
+    Serial.flush();
+    abort();
+  }else{
+    Serial.println("****** Correct status for RTC ******");
+  }
+}
+
+void reloj::get_pins(){
+  Serial.println("-- For developers --");
+  Serial.print("Pins for clock functions -> ");
+  Serial.print("pin sda: ");
+  Serial.print(pin_sda);
+  Serial.print(", pin scl: ");
+  Serial.println(pin_scl);
+}
+
+void reloj::set_time(){
+  DateTime old_time = rtc.now();
+  rtc.adjust(DateTime(__DATE__, __TIME__));
+  DateTime new_time = rtc.now();
+
+  Serial.println("New time has been set");
+  Serial.print("Old time: ");
+  Serial.println(String("DateTime::TIMESTAMP_FULL:\t ")+old_time.timestamp(DateTime::TIMESTAMP_FULL));
+  Serial.print("New time: ");
+  Serial.println(String("DateTime::TIMESTAMP_FULL:\t ")+new_time.timestamp(DateTime::TIMESTAMP_FULL));
+}
+
+static DateTime reloj::get_time(){
+  return rtc.now();
+}
+
+static void reloj::print_time(DateTime time){
+  Serial.print("Current time: ");
+  Serial.print(time.day() / 10);
+  Serial.print(time.day() % 10);
+  Serial.print("/");
+  Serial.print(time.month() / 10);
+  Serial.print(time.month() % 10);
+  Serial.print("/");
+  Serial.print(time.year());
+  Serial.print(" - ");
+
+  Serial.print(time.hour() / 10);
+  Serial.print(time.hour() % 10);
+  Serial.print(":");
+  Serial.print(time.minute() / 10);
+  Serial.print(time.minute() % 10);
+  Serial.print(":");
+  Serial.print(time.second() / 10);
+  Serial.println(time.second() % 10);
+}
---
+++ src/main/reloj.h
@@ -0,0 +1,14 @@
+// reloj
+#include "Arduino.h"
+#include <RTClib.h>
+
+class reloj{
+  private:
+    static const int pin_sda = 20, pin_scl = 21;
+  public:
+    static void check_status();
+    static void get_pins();
+    static void set_time();
+    static DateTime get_time();
+    static void print_time(DateTime time);
+};
